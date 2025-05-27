93a898e853b52d32184e23701410fbe9d51652ff
Firefly-star <firefly120099@gmail.com>
2025-05-27 15:52:42 +0800

    Timer

---
+++ Timer.cpp
@@ -0,0 +1,63 @@
+#include <chrono>
+#include <stdexcept>
+
+using namespace std::chrono;
+
+class Timer
+{
+public:
+	~Timer()
+	{
+		end();
+	}
+	void start() 
+	{
+		if (is_running)
+			throw std::runtime_error("The timer cannot be repeatedly started when it has already started");
+
+		start_time = high_resolution_clock::now();
+		is_running = true;
+	}
+
+	void end() 
+	{
+		if (!is_running)
+			throw std::runtime_error("The timing cannot be ended before it starts");
+
+		auto end_time = high_resolution_clock::now();
+		total_duration += end_time - start_time;
+		is_running = false;
+	}
+
+	void print() const 
+	{
+		auto duration_ms = duration_cast<chrono::milliseconds>(total_duration);
+		cout << "Total time: " << duration_ms.count() << "ms" << endl;
+	}
+
+	void reset() 
+	{
+		total_duration = nanoseconds::zero();
+		is_running = false;
+	}
+private:
+	time_point<high_resolution_clock> start_time;
+	nanoseconds total_duration{ 0 };
+	bool is_running = false;
+};
+
+class ScopedTimer
+{
+public:
+	ScopedTimer()
+	{
+		measure.start();
+	}
+	~ScopedTimer()
+	{
+		measure.end();
+		measure.print();
+	}
+private:
+	Timer timer;
+};
